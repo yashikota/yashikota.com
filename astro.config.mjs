ecc7b7879b15373e7cd3f97d14731ff1d5fbb3dd
kota <52403688+yashikota@users.noreply.github.com>
2023-11-22 04:04:23 +0900

    add panda css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import pandacss from '@pandacss/astro'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [pandacss()]
+})
