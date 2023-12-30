be8abab00e82b62e7441c2007a0eb90f1205394d
kota <52403688+yashikota@users.noreply.github.com>
2023-12-31 08:05:15 +0900

    add react and swiper

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config"
 import pandacss from "@pandacss/astro"
 
+import react from "@astrojs/react"
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [pandacss()],
+    integrations: [pandacss(), react()],
 })
