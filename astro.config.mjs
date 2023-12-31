fd6cc6aff1d7307919af2110157295c70a54336f
kota <52403688+yashikota@users.noreply.github.com>
2024-01-01 06:41:44 +0900

    change redirects method

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,22 @@ import react from "@astrojs/react"
 // https://astro.build/config
 export default defineConfig({
     integrations: [pandacss(), react()],
+    redirects: {
+        "/blogs": {
+            status: 301,
+            destination: "/blog",
+        },
+        "/slide": {
+            status: 301,
+            destination: "/slides",
+        },
+        "/work": {
+            status: 301,
+            destination: "/works",
+        },
+        "/game": {
+            status: 301,
+            destination: "/games",
+        },
+    },
 })
