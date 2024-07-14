8de70a3b0e85185eb3151e60b2702c3470bceef5
yashikota <52403688+yashikota@users.noreply.github.com>
2024-07-15 01:34:41 +0900

    update gallery

--- src/components/instagram.tsx
+++ src/components/instagram.tsx
@@ -1,17 +1,32 @@
-type InstagramEmbedProps = {
+import { useEffect } from "react";
+
+interface InstagramEmbedProps {
     postId: string;
-};
+}
 
 export function InstagramEmbedComponent({ postId }: InstagramEmbedProps) {
-    return (
-        <>
-            <script async src="//www.instagram.com/embed.js"></script>
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "//www.instagram.com/embed.js";
+        script.async = true;
+        document.body.appendChild(script);
 
-            <blockquote
-                className="instagram-media"
-                data-instgrm-permalink={`https://www.instagram.com/p/${postId}`}
-                style={{ maxWidth: "540px", width: "100%" }}
-            ></blockquote>
-        </>
+        const scriptCallback = () => {
+            (window as any).instgrm.Embeds.process();
+        };
+
+        script.onload = scriptCallback;
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, [postId]);
+
+    return (
+        <blockquote
+            className="instagram-media"
+            data-instgrm-permalink={`https://www.instagram.com/p/${postId}`}
+            style={{ maxWidth: "550px", width: "100%" }}
+        ></blockquote>
     );
 }
