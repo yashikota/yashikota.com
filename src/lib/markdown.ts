ef913c3bc25de71b9f60b3c621695b4462550884
yashikota <52403688+yashikota@users.noreply.github.com>
2025-05-28 10:37:44 +0900

    feat: code block

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,5 +1,7 @@
+import rehypeExpressiveCode from "rehype-expressive-code";
 import rehypeSanitize from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
@@ -9,13 +11,15 @@ import { unified } from "unified";
  * @param markdown マークダウン文字列
  * @returns HTML文字列
  */
-export function markdownToHtml(markdown: string): string {
-  const result = unified()
+export async function markdownToHtml(markdown: string): Promise<string> {
+  const result = await unified()
     .use(remarkParse)
+    .use(remarkGfm)
     .use(remarkRehype)
     .use(rehypeSanitize)
+    .use(rehypeExpressiveCode)
     .use(rehypeStringify)
-    .processSync(markdown);
+    .process(markdown);
 
   return result.toString();
 }
