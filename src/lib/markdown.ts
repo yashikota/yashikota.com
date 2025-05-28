7ef7b8bdf6622033d1a31057c6b4f5719fdb9c03
yashikota <52403688+yashikota@users.noreply.github.com>
2025-05-28 11:02:38 +0900

    feat: math support

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,7 +1,9 @@
 import rehypeExpressiveCode from "rehype-expressive-code";
+import rehypeKatex from "rehype-katex";
 import rehypeSanitize from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
@@ -15,8 +17,10 @@ export async function markdownToHtml(markdown: string): Promise<string> {
   const result = await unified()
     .use(remarkParse)
     .use(remarkGfm)
+    .use(remarkMath)
     .use(remarkRehype)
     .use(rehypeSanitize)
+    .use(rehypeKatex)
     .use(rehypeExpressiveCode)
     .use(rehypeStringify)
     .process(markdown);
